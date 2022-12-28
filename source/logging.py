afbf6c74e948a95412b5c939b36b438043860a83
Martin Lanser <martinlanser@gmail.com>
2022-12-28 16:01:07 -0500

    include initial sample code

--- source/logging.py
+++ source/logging.py
@@ -1,3 +1,5 @@
-# SPDX-FileCopyrightText: 2022 Martin Lanser
+# Copyright: 2022 Martin Lanser
+# License: MIT
 #
-# SPDX-License-Identifier: MIT
+# Credits: Based on tutorial by Richard Albritton for Adafruit Industries
+# Reference: https://learn.adafruit.com/making-a-pyportal-user-interface-displayio  
